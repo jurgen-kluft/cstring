c20807d061d626f625f0da686165e3905c99c4a5
Jurgen Kluft <jurgen@virtuosgames.com>
2024-01-24 18:04:31 +0800

    * consistency

--- source/main/include/cstring/c_string.h
+++ source/main/include/cstring/c_string.h
@@ -21,12 +21,12 @@ namespace ncore
         //@TODO: We should also add wchar_t (utf16)
         string_t(const char* str);
         string_t(s32 _len, s32 _type);
-        string_t(string_t const& other); // copy constructor
-        string_t(string_t const& other, string_t const& concat);
+        string_t(const string_t& other); // copy constructor
+        string_t(const string_t& other, const string_t& concat);
         ~string_t();
 
-        s32 format(string_t const& format, const va_t* argv, s32 argc);
-        s32 formatAdd(string_t const& format, const va_t* argv, s32 argc);
+        s32 format(const string_t& format, const va_t* argv, s32 argc);
+        s32 formatAdd(const string_t& format, const va_t* argv, s32 argc);
 
         s32      cap() const;
         s32      size() const;
@@ -54,7 +54,7 @@ namespace ncore
 
         void attach(string_t& str);
         void release();
-        void clone(string_t const& str);
+        void clone(const string_t& str);
 
         void add_to_list(string_t* node) const;
         void rem_from_list();
@@ -77,8 +77,8 @@ namespace ncore
     uchar32 firstChar(const string_t&);
     uchar32 lastChar(const string_t&);
 
-    bool startsWith(const string_t&, string_t const& inStartStr);
-    bool endsWith(const string_t&, string_t const& inEndStr);
+    bool startsWith(const string_t&, const string_t& inStartStr);
+    bool endsWith(const string_t&, const string_t& inEndStr);
 
     ///@name Comparison
     s32  compare(const string_t& inLHS, const string_t& inRHS);
@@ -86,13 +86,13 @@ namespace ncore
     bool contains(const string_t& inStr, uchar32 inContains);
     bool contains(const string_t& inStr, const string_t& inContains);
 
-    s32 format(string_t&, string_t const& formatString, const va_list_t& args);
-    s32 formatAdd(string_t&, string_t const& formatString, const va_list_t& args);
+    s32 format(string_t&, const string_t& formatString, const va_list_t& args);
+    s32 formatAdd(string_t&, const string_t& formatString, const va_list_t& args);
 
     void upper(string_t& inStr);
     void lower(string_t& inStr);
     void capitalize(string_t& inStr);
-    void capitalize(string_t& inStr, string_t const& inSeperators);
+    void capitalize(string_t& inStr, const string_t& inSeperators);
 
     string_t selectUntil(const string_t& inStr, uchar32 inFind);
     string_t selectUntil(const string_t& inStr, const string_t& inFind);
@@ -109,18 +109,18 @@ namespace ncore
     string_t find(string_t& inStr, uchar32 inFind);
     string_t find(string_t& inStr, const char* inFind);
     string_t find(string_t& inStr, const string_t& inFind);
-    string_t findLast(string_t const& inStr, const string_t& inFind);
+    string_t findLast(const string_t& inStr, const string_t& inFind);
     string_t findOneOf(string_t& inStr, const string_t& inFind);
     string_t findOneOfLast(string_t& inStr, const string_t& inFind);
 
-    void insert(string_t&, string_t const& pos, string_t const& insert);
-    void insert_after(string_t&, string_t const& pos, string_t const& insert);
+    void insert(string_t&, const string_t& pos, const string_t& insert);
+    void insert_after(string_t&, const string_t& pos, const string_t& insert);
 
-    void remove(string_t&, string_t const& selection);
+    void remove(string_t&, const string_t& selection);
     void find_remove(string_t&, const string_t& find);
-    void find_replace(string_t&, string_t const& find, string_t const& replace);
+    void find_replace(string_t&, const string_t& find, const string_t& replace);
     void remove_any(string_t&, const string_t& inAny);
-    void replace_any(string_t&, string_t const& inAny, uchar32 inWith);
+    void replace_any(string_t&, const string_t& inAny, uchar32 inWith);
 
     void trim(string_t&);
     void trimLeft(string_t&);
@@ -128,9 +128,9 @@ namespace ncore
     void trim(string_t&, uchar32 inChar);
     void trimLeft(string_t&, uchar32 inChar);
     void trimRight(string_t&, uchar32 inChar);
-    void trim(string_t&, string_t const& inCharSet);
-    void trimLeft(string_t&, string_t const& inCharSet);
-    void trimRight(string_t&, string_t const& inCharSet);
+    void trim(string_t&, const string_t& inCharSet);
+    void trimLeft(string_t&, const string_t& inCharSet);
+    void trimRight(string_t&, const string_t& inCharSet);
     void trimQuotes(string_t&);
     void trimQuotes(string_t&, uchar32 quote);
     void trimDelimiters(string_t&, uchar32 inLeft, uchar32 inRight);
@@ -143,7 +143,7 @@ namespace ncore
     bool splitOnLast(string_t&, string_t& inStr, string_t& outLeft, string_t& outRight);
 
     void concatenate(string_t& str, const string_t& con);
-    void concatenate_repeat(string_t&, string_t const& con, s32 ntimes);
+    void concatenate_repeat(string_t&, const string_t& con, s32 ntimes);
 
 } // namespace ncore
 
